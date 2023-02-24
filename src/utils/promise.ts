57130fd26afca7cd88c910761456565d6f12ced5
OctupusPrime <rasiapypkin2014@gmail.com>
2023-02-24 16:49:17 +0200

    fix retry count

--- src/utils/promise.ts
+++ src/utils/promise.ts
@@ -16,12 +16,12 @@ async function hadleRetry<T>(
   promise: Promise<T>,
   retry: number = 5
 ): Promise<[Awaited<T> | null, any]> {
-  if (retry <= 0) return [null, true];
+  if (retry < 0) return [null, true];
 
   const [data, err] = await handler<T>(promise);
 
   if (err) {
-    if (retry === 1) return [data, err];
+    if (retry === 0) return [data, err];
 
     return await hadleRetry(promise, retry - 1);
   }
